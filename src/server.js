6513dbe6b5c97ba9ab0fb919ebd91f4fa1bdb960
bernini <bernini@inventati.org>
2019-10-25 10:16:56 +0200

    Site update (autocommit while deploying)

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ const dev = NODE_ENV === 'development';
 
 polka() // You can also use Express
 	.use(
+		'./socialwallet-website-export',
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
 		sapper.middleware()
